58e65ab7b9589fd88ed439381b087ae953e17653
spockfish <htenberge@gmail.com>
2018-05-15 10:49:30 +0200

    some cleaning up

--- server/config.js
+++ server/config.js
@@ -15,31 +15,6 @@ module.exports = {
      return out
   },
 
-  write: function(hostname, reboottime, audio, audio_usb, timezone, touchscreen_detected, touchscreen_orientation, touchscreen_zone, auto_update, repo) {
-     var p = {}
-     p.rp_hostname = hostname;
-     p.rp_reboottime = reboottime;
-     p.rp_auto_update = auto_update;
-     p.rp_audio = audio;
-     p.rp_audio_usb = audio_usb;
-     p.rp_timezone = timezone;
-     p.rp_touchscreen_detected = touchscreen_detected;
-     p.rp_touchscreen_orientation = touchscreen_orientation;
-     zone = '\'' + touchscreen_zone + '\'';
-     p.rp_touchscreen_zone = zone;
-     p.rp_repo = repo;
-
-     var tmpfile = fs.mkdtempSync('/tmp/ropieee');
-
-     console.log('tmpfile = ' + tmpfile);
-     tmpfile = tmpfile + '/settings.ini';
-     console.log('config:write: writing to ' + tmpfile);
-
-     fs.writeFileSync(tmpfile, ini.stringify(p, {}));
-
-     return tmpfile;
-  },
-
   write2: function(obj_settings) {
      var tmpfile = fs.mkdtempSync('/tmp/ropieee');
 
